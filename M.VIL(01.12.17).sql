1190663000fc9f45541ddc50193fd5698584f8d4
MaryVil <79069844555@yandex.ru>
2017-12-02 02:14:47 +0700

    .

--- M.VIL(01.12.17).sql
+++ M.VIL(01.12.17).sql
@@ -1,50 +1,7 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 9.6.5
--- Dumped by pg_dump version 9.6.5
-
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
-DROP DATABASE "M.VIL";
-
-CREATE DATABASE "M.VIL" WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'Russian_Russia.1251' LC_CTYPE = 'Russian_Russia.1251';
-
-
-ALTER DATABASE "M.VIL" OWNER TO postgres;
-
-\connect "M.VIL"
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-SET search_path = public, pg_catalog;
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- Name: hierarchy; Type: TYPE; Schema: public; Owner: postgres
+DROP DATABASE IF EXISTS M.VIL;
+CREATE DATABASE M.VIL;
+GRANT ALL PRIVILEGES ON DATABASE M.VIL TO postgres;
+\connect M.VIL
 --
 
 CREATE TYPE hierarchy AS ENUM (
